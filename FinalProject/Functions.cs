af184428e465a1e5f80831d52761f13c16a41bec
Nyxilian <whamin1209@gmail.com>
2024-11-06 12:03:27 -0600

    초기 커밋.

---
+++ FinalProject/Functions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace FinalProject
+{
+    public class Functions
+    {
+        public static MySqlConnection ConnectDB()
+        {
+            string connStr = "server=127.0.0.1;uid=root;pwd=Nyxilian@0908;database=its245final";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            conn.Open();
+            return conn;
+        }
+
+        public static DataTable LoadTable(string sqlQuery, MySqlConnection conn)
+        {
+            MySqlCommand cmd = new MySqlCommand();
+            DataTable dt = new DataTable();
+            try
+            {
+                cmd = new MySqlCommand(sqlQuery, conn);
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                da.Fill(dt);
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                cmd.Dispose();
+            }
+            MessageBox.Show("Unable to retrieve data from DB.");
+            return null;
+        }
+    }
+}
---
+++ FinalProject/SelectPatient.cs
@@ -0,0 +1,28 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProject
+{
+    public partial class SelectPatient : Form
+    {
+        MySqlConnection conn;
+        public SelectPatient()
+        {
+            InitializeComponent();
+        }
+        private void SelectPatient_Load(object sender, EventArgs e)
+        {
+            conn = Functions.ConnectDB();
+            string query = "SELECT * FROM its245final.patientdemographics;";
+            dataGridView1.DataSource = Functions.LoadTable(query, conn);
+        }
+    }
+}
